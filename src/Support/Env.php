1949792b3b8e779f95873988701881aa4704ad78
clarksye <burhanbasyri@gmail.com>
2022-02-18 15:06:52 -0800

    add support\Env

--- src/Support/Env.php
+++ src/Support/Env.php
@@ -3,8 +3,22 @@
 namespace VIRUS\Framework\Support;
 
 
-
 class Env
 {
- 
+
+    public static function env(string $search): string
+    {
+        $lines = file(__DIR__.'/.env');
+        foreach ($lines as $line) {
+            // explode the line into an array
+            $values = explode('=', $line);
+            // trim the whitspace from the value
+            if (trim($values[0]) == $search) {
+                array_shift($values);
+                return trim(implode('=', $values));
+                // exit the foreach if we found the needle
+                break;
+            }
+        }
+    }
 }
